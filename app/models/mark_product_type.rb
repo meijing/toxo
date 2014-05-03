1a73c5071c8c5978b51580d45be1800a7d304ff8
paula <paula@paula-HP-G62-Notebook-PC.(none)>
2014-05-03 11:15:42 +0200

    borrado de archivos

--- app/models/mark_product_type.rb
+++
@@ -1,4 +0,0 @@
-class MarkProductType < ActiveRecord::Base
-  belongs_to :mark
-  belongs_to :product_type
-end
--- db/migrate/20140419151113_add_field_image_to_product.rb
+++
@@ -1,5 +0,0 @@
-class AddFieldImageToMark < ActiveRecord::Migration
-  def change
-    add_attachment :products, :image
-  end
-end
