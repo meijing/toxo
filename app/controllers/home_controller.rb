855c0a240bbc02a0ab45f8cbebb6afe0de48c863
paula <paula@paula-HP-G62-Notebook-PC.(none)>
2014-06-12 18:46:30 +0200

    filtro fotos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -29,6 +29,13 @@ class HomeController < ApplicationController
 
   def products
 
+    if !params[:mark_id].nil? and !params[:product_type_id].nil? and !params[:category_id].nil?
+      @mark = Mark.find(params[:mark_id])
+      @product_type = ProductType.find(params[:product_type_id])
+      @category = Category.find(params[:category_id])
+      @products = Product.where('mark_id = ? and product_type_id = ? and mark_id=?', @mark.id, @product_type.id, @category.id)
+    end
+    
     if !params[:mark_id].nil? and !params[:category_id].nil?
       @mark = Mark.find(params[:mark_id])
       @category = Category.find(params[:category_id])
@@ -44,13 +51,6 @@ class HomeController < ApplicationController
       @mark = Mark.find(params[:mark_id])
       @product_type = ProductType.find(params[:product_type_id])
       @products = Product.where('mark_id = ? and product_type_id = ?', @mark.id, @product_type.id)
-    end
-
-    if !params[:mark_id].nil? and !params[:product_type_id].nil? and !params[:category_id].nil?
-      @mark = Mark.find(params[:mark_id])
-      @product_type = ProductType.find(params[:product_type_id])
-      @category = Category.find(params[:category_id])
-      @products = Product.where('mark_id = ? and product_type_id = ? and mark_id=?', @mark.id, @product_type.id, @category.id)
     end
 
     if !params[:category_id].nil? and !params[:product_type_id].nil?
