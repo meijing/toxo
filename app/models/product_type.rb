0bccd6c67026c899b8307fa88c48e5f33107723c
paula <paula@paula-HP-G62-Notebook-PC.(none)>
2014-06-12 18:40:24 +0200

    problema no filtrado

--- app/models/product_type.rb
+++ app/models/product_type.rb
@@ -6,8 +6,8 @@ class ProductType < ActiveRecord::Base
   has_many :mark, :through=> :mark_product_types
 
   def self.find_product_types(mark_id, category_id, product_type_id)
-    ProductType.joins('join mark_product_types mpt on mpt.product_type_id = product_types.id').
+      ProductType.joins('join mark_product_types mpt on mpt.product_type_id = product_types.id').
       joins('join products p on p.product_type_id = product_types.id and p.category_id = mpt.category_id').
-      where('(p.mark_id = ? or ? is null) and (p.category_id = ? or ? is null) ', mark_id, mark_id, category_id, category_id).uniq.order(:name)
+      where('(p.mark_id = ? or ? is null) and (p.category_id = ? or ? is null) and (p.product_type_id = ? or ? is null)', mark_id, mark_id, category_id, category_id, product_type_id, product_type_id).uniq.order(:name)
   end
 end
