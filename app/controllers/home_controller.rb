5a04063e3ff585b208372fbd65249fdb8a122284
paula <paula@paula-HP-G62-Notebook-PC.(none)>
2014-06-16 23:22:46 +0200

    arreglo no filtro

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -36,30 +36,30 @@ class HomeController < ApplicationController
       @products = Product.where('mark_id = ? and product_type_id = ? and category_id=?', @mark.id, @product_type.id, @category.id)
     end
     
-    if !params[:mark_id].nil? and !params[:category_id].nil?
+    if !params[:mark_id].nil? and !params[:category_id].nil? and params[:product_type_id].nil?
       @mark = Mark.find(params[:mark_id])
       @category = Category.find(params[:category_id])
       @products = Product.where('mark_id = ? and category_id = ?', @mark.id, @category.id)
     end
 
-    if !params[:mark_id].nil? and params[:product_type_id].nil?
+    if !params[:mark_id].nil? and params[:product_type_id].nil? and params[:category_id].nil?
       @mark = Mark.find(params[:mark_id])
       @products = Product.where('mark_id = ?', @mark.id)
     end
 
-    if !params[:mark_id].nil? and !params[:product_type_id].nil?
+    if !params[:mark_id].nil? and !params[:product_type_id].nil? and params[:category_id].nil?
       @mark = Mark.find(params[:mark_id])
       @product_type = ProductType.find(params[:product_type_id])
       @products = Product.where('mark_id = ? and product_type_id = ?', @mark.id, @product_type.id)
     end
 
-    if !params[:category_id].nil? and !params[:product_type_id].nil?
+    if !params[:category_id].nil? and !params[:product_type_id].nil? and params[:mark_id].nil?
       @category = Category.find(params[:category_id])
       @product_type = ProductType.find(params[:product_type_id])
       @products = Product.where('category_id = ? and product_type_id = ?', @category.id, @product_type.id)
     end
 
-    if !params[:category_id].nil? and params[:product_type_id].nil?
+    if !params[:category_id].nil? and params[:product_type_id].nil? and params[:mark_id].nil?
       @category = Category.find(params[:category_id])
       @products = Product.where('category_id = ?', @category.id)
     end
