1b19ff4318fde4f6c9f085f0759c69d9e9d486ad
paula <paula@paula-HP-G62-Notebook-PC.(none)>
2014-06-24 10:59:19 +0200

    arreglos para as promocions

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,7 +1,22 @@
 class Promotion < ActiveRecord::Base
 
+  validates :description, :presence => true
+  validates :text, :presence => true
+  validates :style, :presence => true
+  validates :date_start, :presence => true
+  validates :date_end, :presence => true, :if => :presence_date_end?
+  validates :end_stocks, :presence => true, :if => :presence_end_stocks?
+
   scope :is_active, lambda {
-    where('CURRENT_DATE >= date_start and date_end >= CURRENT_DATE')
+    where('CURRENT_DATE >= date_start and (date_end >= CURRENT_DATE or end_stocks = 1)')
   }
+
+  def presence_date_end?
+    !date_start.nil? and end_stocks.nil?
+  end
+
+  def presence_end_stocks?
+    !date_start.nil? and date_end.nil?
+  end
 
 end
