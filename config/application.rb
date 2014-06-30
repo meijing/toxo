6b9856bdb63715e5f545e2f5bdd9bec03364abd9
paula <paula@paula-HP-G62-Notebook-PC.(none)>
2014-06-30 18:50:35 +0200

    limpieza assets

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module Toxo
 
     #config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    
      #config.assets.paths << "#{Rails.root}/app/assets/fonts"
      config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
   end
