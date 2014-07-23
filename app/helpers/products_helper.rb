3e22507f37c841ecb7704f8369c90d4f46c8490d
paula <paula@paula-HP-G62-Notebook-PC.(none)>
2014-07-23 18:41:02 +0200

    marcar producto en promocion

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,29 @@
 module ProductsHelper
+
+  def show_product_in_promotion product
+    Promotion.is_active.each do |promotion|
+      if !promotion.catalog.nil? && promotion.catalog != '' &&
+          promotion.catalog == product.name
+        return true
+      end
+
+      if !promotion.mark_id.nil? &&
+          promotion.mark_id == product.mark_id
+        return true
+      end
+
+      if !promotion.product_type_id.nil? &&
+          !promotion.category_type_id.nil? &&
+          promotion.product_type_id == product.product_type_id &&
+          promotion.category_type_id == product.category_id
+        p '---------------------------'
+        p promotion.product_type_id
+        p  promotion.category_type_id
+        p product.product_type_id
+        p product.category_id
+        return true
+      end
+    end
+    return false
+  end
 end
