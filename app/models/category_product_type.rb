2059f226db277b2b80e3e086d04698c4d1e0ff77
paula <paula@paula-HP-G62-Notebook-PC.(none)>
2014-07-23 16:39:08 +0200

    proba a modificar os tipos de productos rebaixados

--- app/models/category_product_type.rb
+++ app/models/category_product_type.rb
@@ -3,6 +3,6 @@ class CategoryProductType < ActiveRecord::Base
   belongs_to :product_type
 
   def self.get_relations product_type_id
-    CategoryProductType.where('product_type_id = ?', product_type_id)
+    CategoryProductType.where('product_type_id = ? and sale = ?', product_type_id, 1)
   end
 end
