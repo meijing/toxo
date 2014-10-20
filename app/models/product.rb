cb7dd11e940ca468643a3d0abb18bed76986aee6
paula <paula@paula-HP-G62-Notebook-PC.(none)>
2014-10-20 22:21:47 +0200

    ampliar foto max

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  has_attached_file :image, styles: {max: '1200x900', original: '600x450', medium: '200x200>', thumb: '48x48>' }, :default_url => "/images/thumb/missing.png"
+  has_attached_file :image, styles: {max: '3000x2250', original: '600x450', medium: '200x200>', thumb: '48x48>' }, :default_url => "/images/thumb/missing.png"
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
   belongs_to :mark
 
