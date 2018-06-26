ed21dfbc2ddb9f64224451a5c431c803243d8ccf
Meijing <caminopaula@gmail.com>
2018-06-26 19:40:12 +0000

    ordear rebaixas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -74,9 +74,9 @@ class HomeController < ApplicationController
     @pt_id = params[:product_type_id]
     @cat_id = params[:category_id]
     if !@cat_id.nil?
-      @products = Product.where('sale = ? and (outlet is null or outlet = 0) and (? is null or product_type_id=?) and category_id = ?  and (new_collection is null or new_collection = 0)', 1, @pt_id, @pt_id, @cat_id)
+      @products = Product.where('sale = ? and (outlet is null or outlet = 0) and (? is null or product_type_id=?) and category_id = ?  and (new_collection is null or new_collection = 0)', 1, @pt_id, @pt_id, @cat_id).order(:name)
     else
-      @products = Product.where('sale = ? and (outlet is null or outlet = 0) and (new_collection is null or new_collection = 0)', 1).sample(15)
+      @products = Product.where('sale = ? and (outlet is null or outlet = 0) and (new_collection is null or new_collection = 0)', 1).order(:name).sample(15)
     end
   end
 
