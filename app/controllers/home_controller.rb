858984891995845f7c34e3c05b7551b27d887946
Meijing <caminopaula@gmail.com>
2018-06-27 19:28:26 +0000

    orden rebaixas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -76,7 +76,7 @@ class HomeController < ApplicationController
     if !@cat_id.nil?
       @products = Product.where('sale = ? and (outlet is null or outlet = 0) and (? is null or product_type_id=?) and category_id = ?  and (new_collection is null or new_collection = 0)', 1, @pt_id, @pt_id, @cat_id).order(:name)
     else
-      @products = Product.where('sale = ? and (outlet is null or outlet = 0) and (new_collection is null or new_collection = 0)', 1).limit(15).order('random()').order(:name) 
+      @products = Product.where('sale = ? and (outlet is null or outlet = 0) and (new_collection is null or new_collection = 0)', 1).limit(5).order('random()').order('name') 
     end
   end
 
