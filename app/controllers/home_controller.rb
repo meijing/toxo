13be520dc2875665fd2e2d4964697a74e48af868
Meijing <caminopaula@gmail.com>
2018-07-04 15:28:17 +0000

    ordenar rebaixas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -74,9 +74,9 @@ class HomeController < ApplicationController
     @pt_id = params[:product_type_id]
     @cat_id = params[:category_id] 
     if !@cat_id.nil?
-      @products = Product.where('sale = ? and (outlet is null or outlet = 0) and (? is null or product_type_id=?) and category_id = ?  and (new_collection is null or new_collection = 0)', 1, @pt_id, @pt_id, @cat_id).order(:name)
+      @products = Product.joins(:mark).where('sale = ? and marks.hidden = 0 and (outlet is null or outlet = 0) and (? is null or product_type_id=?) and category_id = ?  and (new_collection is null or new_collection = 0)', 1, @pt_id, @pt_id, @cat_id).order('marks.name, products.name')
     else
-      @products = Product.where('sale = ? and (outlet is null or outlet = 0) and (new_collection is null or new_collection = 0)', 1).limit(15).order('random(),name')
+      @products = Product.joins(:mark).where('sale = ? and marks.hidden = 0 and (outlet is null or outlet = 0) and (new_collection is null or new_collection = 0)', 1).limit(15).order('random(),name')
     end
   end
 
